97463ff03cb7a840c309972ba067cc113baac9a1
georgesoft07 <george_soft_01@hotmail.com>
2017-09-27 19:34:44 -0500

    first commit

---
+++ CapaPresentacion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data.Entity;
+
+namespace CapaPresentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BDDISTRIBUCIONEntities db = new BDDISTRIBUCIONEntities();
+            RUTA rutas1 = new RUTA();
+            rutas1.CODIGO ="6";
+            rutas1.CODEMPRESA = "01";
+            rutas1.DESCRIPCION = "fff";
+            db.RUTA.Add(rutas1);
+            db.SaveChanges();
+
+        }
+    }
+}
